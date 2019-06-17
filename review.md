50a5f998a2cae2e3dbb36de285a9228dfc1fcaa4
AndGasper <24193043+AndGasper@users.noreply.github.com>
2019-06-16 22:25:56 -0700

    Updated the format of the subscriptions; haven't tried to deploy it yet; added inkling of the template bucket.

---
+++ review.md
@@ -0,0 +1,44 @@
+## Wrong.
+It should be: 
+- someone uploads a new valid sgml file to a `content` bucket
+- this emits an event to lambda to fetch the subscription
+
+1. The CloudFormation template should:
+    - define the content bucket
+    - define the lambda function
+    - output
+        - lambda
+        - s3 content bucket
+2. There should be a _second_ cloudformation template that is a change set
+    - 
+
+
+## Why is this wrong? ...is it?
+- Why should it be:
+```
+s3 -event-> lambda 
+```
+```
+-cloudwatch event-> lambda -> s3
+```
+
+Hmm..
+I think an aggregator can run on a schedule, but a reader should probably be triggered 
+
+
+- I'll need to mull this one over because 
+
+If:
+```
+GET: Query -> Handler - Response
+POST: Command -> Handler -> Response 
+```
+then that's probably why the idea of:
+---------
+| aether | -event-> lambda -GET-> 
+----------
+then -GET-> (again) 
+for the template thereby changing the global state makes me raise an eyebrow.
+
+
+[AWS CloudFormation S3 Bucket Notifications](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-notificationconfig.html)
--- src/fetchSubscriptions.js
+++ src/fetchSubscriptions.js
@@ -1,5 +1,83 @@
 const AWS = require('aws-sdk');
+const http = requre('http');
 
 exports.handler = (event,  context, callback) => {
-    
+    const debugging = {
+        'event': event,
+        'context': context,
+        'callback': callback
+    };
+    console.log('fetchSubscriptions.js - debugging:', debugging);
+    /**
+     * @name getSubscriptions
+     * @description - 
+     */
+    function getSubscriptions() {
+        // this could probably be abstracted to some type of environment variable/config
+        const options = {
+            hostname: 'wakemygameup.com',
+            port: 80,
+            path: '/feeds/subscriptions.json',
+        };
+        let subscriptions;
+        // get the published list of subscriptions
+        http.get(options, function(response) {
+            if (response) {
+                try {
+                    // this may be unnecessary, but it might be  worth adding validation of the response around here
+                    const subscriptionInformation = JSON.parse(response.body);
+                    subscriptionInformation.subscriptions.map(subscription => subscriptions.push(subscription));
+                    
+
+                } catch (error) {
+                    // now what?
+                }
+            }
+        });
+        return subscriptions;
+    }
+    /**
+     * @name convertXmlToDom
+     * @param {String} xml - the xml 
+     * @description - A way of hiding potential error handling 
+     */
+    function convertXmlToDom(xml) {
+        const objectParser = new DOMParser();
+        const objectDom = objectParser.parseFromString(xml, "application/xml");
+        return objectDom;
+    }
+    /**
+     * @name fetchSubscriptionContent
+     * @param {Object} subscription 
+     *  - name {String}
+     *  - type {String}
+     *  - path {String}
+     * @description - 
+     */
+    function fetchSubscriptionContent(subscription) {
+        // pick off the path information from the subscription
+        const subscriptionSource = new URL(subscription.url);
+        const { path, name, type } = subscription;
+        const options = {
+            hostname: subscriptionSource.hostname,
+            path: subscriptionSource.pathname
+        };
+        http.get(options, function(response) {
+            // domain logic starts kicking in around here
+            if (response.status === 200) {
+                
+            }
+        });
+        return subscriptionContent;
+    }
+
+    if (event) {
+        const subscriptions = getSubscriptions();
+        // implicit dependence on subscriptions being iterable having a .length property
+        if (subscriptions.length) {
+            const subscriptionsContent = subscriptions.map(subscription => fetchSubscriptionContent(subscription));
+            console.log('subscriptionsContent', subscriptionsContent);
+        }
+    }
+
 }
